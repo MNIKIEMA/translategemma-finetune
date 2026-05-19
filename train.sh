fb8516e0ce9821f3c335a9ad762e2a210956d714
MNIKIEMA <nikiemamahamadi01@gmail.com>
2026-05-19 14:34:23 +0200

    Tune training script defaults

--- train.sh
+++ train.sh
@@ -7,6 +7,13 @@ export PYTHONUNBUFFERED="${PYTHONUNBUFFERED:-1}"
 
 uv run translategemma-finetune \
   --use_chat_template true \
-  --chat_template_path translategemma_template.jinja \
+  --chat_template_path chat_template.jinja \
   --fp16 true \
+  --per_device_train_batch_size 1 \
+  --gradient_accumulation_steps 16 \
+  --max_steps -1 \
+  --max_grad_norm 1.0 \
+  --learning_rate 1e-5 \
+  --optim adamw_torch \
+  --test_prompt "Bonjour, comment allez-vous ?" \
   "$@"
